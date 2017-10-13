d08c559c0593f753f595143b1552354d6798962d
John Tuttle <thejwt@gmail.com>
2017-10-13 22:36:54 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,14 @@
+INSERT INTO series (title,author_id,subgenre_id)
+VALUES("Harry Potter",1,1);
+INSERT INTO series (title,author_id,subgenre_id)
+VALUES("Lord of the Rings",2,2);
+
+INSERT INTO subgenres(name)
+VALUES("Young-Adult");
+INSERT INTO subgenres(name)
+VALUES("Epic");
+
+INSERT INTO authors(name)
+VALUES("J.K. Rowling");
+INSERT INTO authors(name)
+VALUES("J.R.R. Tolkien");
