a04832b97ac25c55cd5f89552fec9a1925cb4159
John Tuttle <thejwt@gmail.com>
2017-10-13 22:57:23 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -45,20 +45,20 @@ VALUES("Gandalf","All we have to decide is what to do with the time that is give
 INSERT INTO characters(name,motto,species,author_id,series_id)
 VALUES("Boromir","The Ring! Is it not a strange fate that we should suffer so much fear and doubt for so small a thing? So small a thing!","Man",2,2);
 
-INSERT INTO character_books (character_id, book_id)VALUES(1,1)
-INSERT INTO character_books (character_id, book_id)VALUES(1,2)
-INSERT INTO character_books (character_id, book_id)VALUES(1,3)
-INSERT INTO character_books (character_id, book_id)VALUES(2,1)
-INSERT INTO character_books (character_id, book_id)VALUES(2,2)
-INSERT INTO character_books (character_id, book_id)VALUES(2,3)
-INSERT INTO character_books (character_id, book_id)VALUES(3,2)
-INSERT INTO character_books (character_id, book_id)VALUES(4,1)
+INSERT INTO character_books (character_id, book_id)VALUES(1,1);
+INSERT INTO character_books (character_id, book_id)VALUES(1,2);
+INSERT INTO character_books (character_id, book_id)VALUES(1,3);
+INSERT INTO character_books (character_id, book_id)VALUES(2,1);
+INSERT INTO character_books (character_id, book_id)VALUES(2,2);
+INSERT INTO character_books (character_id, book_id)VALUES(2,3);
+INSERT INTO character_books (character_id, book_id)VALUES(3,2);
+INSERT INTO character_books (character_id, book_id)VALUES(4,1);
 
-INSERT INTO character_books (character_id, book_id)VALUES(6,4)
-INSERT INTO character_books (character_id, book_id)VALUES(6,5)
-INSERT INTO character_books (character_id, book_id)VALUES(6,6)
-INSERT INTO character_books (character_id, book_id)VALUES(7,4)
-INSERT INTO character_books (character_id, book_id)VALUES(7,5)
-INSERT INTO character_books (character_id, book_id)VALUES(7,6)
-INSERT INTO character_books (character_id, book_id)VALUES(5,6)
-INSERT INTO character_books (character_id, book_id)VALUES(8,5)
+INSERT INTO character_books (character_id, book_id)VALUES(6,4);
+INSERT INTO character_books (character_id, book_id)VALUES(6,5);
+INSERT INTO character_books (character_id, book_id)VALUES(6,6);
+INSERT INTO character_books (character_id, book_id)VALUES(7,4);
+INSERT INTO character_books (character_id, book_id)VALUES(7,5);
+INSERT INTO character_books (character_id, book_id)VALUES(7,6);
+INSERT INTO character_books (character_id, book_id)VALUES(5,6);
+INSERT INTO character_books (character_id, book_id)VALUES(8,5);
--- lib/schema.sql
+++ lib/schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE series (
 
 CREATE TABLE subgenres (
   id INTEGER PRIMARY KEY,
-  name TEXT  
+  name TEXT
 );
 
 CREATE TABLE authors (
